f548b660f5d8b41e3b2b8ed499a96e9ba527e348
Tairone Dias <taironecdias@gmail.com>
2019-05-18 17:14:24 -0300

    up selecionar fotos

--- Snapchat/Snapchat/FotoViewController.swift
+++ Snapchat/Snapchat/FotoViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
-class FotoViewController: UIViewController {
-
+class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var campoDescricao: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func btnSelecionarFoto(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imgRecuperada = info[.originalImage] as? UIImage {
+            self.imageView.image = imgRecuperada
+        } else {
+            print("Not able to get an image")
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
